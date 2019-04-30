5b11ed2a3abf086365b97bc636da17e123927b9b
UnoriginalBapple <Basschan08@gmail.com>
2019-04-30 14:30:17 +0200

    New stuff

---
+++ ProgramStructure/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ProgramStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hi there");
+            Console.ReadKey();
+
+            Console.WriteLine ("This moment belongs to me")
+        }
+
+        interface student
+        {
+            void getdata ()
+            {
+                // get data 
+            }
+
+        }
+
+        System.Data.IDbConnection OleDBConnection () { }
+
+    }
+}
