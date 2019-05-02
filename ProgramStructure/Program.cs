f14279bc8884b32971137a4a01d6f6a962dec09e
UnoriginalBapple <Basschan08@gmail.com>
2019-05-02 15:08:29 +0200

    okie

--- ProgramStructure/Program.cs
+++ ProgramStructure/Program.cs
@@ -9,19 +9,35 @@ namespace ProgramStructure
             System.Console.WriteLine("Hi there");
             Console.ReadKey();
 
-            Console.WriteLine ("This moment belongs to me")
+            Console.WriteLine("This moment belongs to me");
         }
 
         interface student
         {
-            void getdata ()
+            void getdata()
             {
                 // get data 
+                Console.WriteLine("Enter the first number\t:");
+                number1 = int.Parse(Console.ReadLine());
+
+
+                int number1;
+                try
+                {
+                    Console.WriteLine("Enter the first number\t:");
+                    number1 = int.Parse(Console.ReadLine());
+                    Console.ReadKey();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error" + e.ToString());
+                    Console.ReadKey();
+                }
             }
 
         }
 
-        System.Data.IDbConnection OleDBConnection () { }
+        // System.Data.IDbConnection OleDBConnection () { }
 
     }
 }
